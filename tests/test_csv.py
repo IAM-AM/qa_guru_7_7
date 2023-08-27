be6a75a84f0d976d0e8796d4d59685276362c15f
IAM-AM <aliaksandr.manko1@gmail.com>
2023-08-28 01:36:40 +0400

    initial commit

---
+++ tests/test_csv.py
@@ -0,0 +1,29 @@
+import csv
+import os.path
+from tests.conftest import RESOURCE_ROOT_PATH
+
+
+# TODO оформить в тест, добавить ассерты и использовать универсальный путь
+
+
+def test_csv_file():
+    csv_path = os.path.join(RESOURCE_ROOT_PATH, 'test_csv.py')
+    names = [
+        ['Bonny', 'Pavel', 'Peter'],
+        ['Alex', 'Serj', 'Yana']
+    ]
+    try:
+        with open(csv_path, "w") as file:
+            csvwriter = csv.writer(file, delimiter=',')
+            csvwriter.writerows(names)
+
+        with open(csv_path) as file:
+            csvreader = csv.reader(file)
+            actual_names = [row for row in csvreader]
+
+        assert actual_names == names, "Ожидаемые имена отсутствуют в списке"
+        assert os.path.exists(csv_path)
+
+    finally:
+        if os.path.exists(csv_path):
+            os.remove(csv_path)
