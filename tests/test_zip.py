d015575f018ce1cd55090c5f12459f74c9a6f391
IAM-AM <aliaksandr.manko1@gmail.com>
2023-08-30 00:46:34 +0400

    Refactored, added tests

--- tests/test_zip.py
+++ tests/test_zip.py
@@ -1,6 +1,7 @@
 import zipfile
 import os
 from conftest import RESOURCE_ROOT_PATH
+import shutil
 
 
 def test_zip_file():
@@ -9,13 +10,39 @@ def test_zip_file():
     with zipfile.ZipFile(file_path) as hellozip:
         hellozip.extract('hello.txt')
 
-        print(hellozip.namelist())
-        text = hellozip.read('hello.txt')
-        text = text.decode('utf-8')
+        listname = hellozip.namelist()
+        text = (hellozip.read('hello.txt').decode('utf-8'))
         print(text)
 
     assert file_path.endswith('.zip')
     assert os.path.isfile(file_path)
-    assert text == str('world')
+    assert listname == ['hello.txt']
+    assert text == 'world'
 
-    os.remove(file_path)
+    # os.remove(file_path)
+
+
+test_zip_file()
+
+
+def test_zip_file_archivation():
+    tmp_dir = "tmp"
+    os.makedirs(tmp_dir, exist_ok=True)
+
+    tmp_path = os.path.join(tmp_dir, "new.zip")
+
+    with zipfile.ZipFile(tmp_path, 'w') as zip_file:
+        for file in [
+            "docs-pytest-org-en-latest.pdf",
+            "file_example_XLS_10.xls",
+            "file_example_XLSX_50.xlsx"
+        ]:
+            file_path = os.path.join(RESOURCE_ROOT_PATH, file)
+            zip_file.write(file_path, os.path.basename(file_path))
+
+            assert file in zip_file.namelist()
+
+        shutil.rmtree(tmp_dir)
+
+
+test_zip_file_archivation()
