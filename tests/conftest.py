5f4ee99a85e32edeec5c12f048a080997577f440
IAM-AM <aliaksandr.manko1@gmail.com>
2023-08-31 22:48:50 +0400

    Added path to file

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,5 +2,6 @@ import pytest
 import csv
 import os
 
-PROJECT_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-RESOURCE_ROOT_PATH = os.path.join(PROJECT_ROOT_PATH, '../resources')
+FILE_PATH = os.path.abspath(__file__)
+PROJECT_ROOT_PATH = os.path.dirname(FILE_PATH)
+RESOURCE_ROOT_PATH = os.path.join(PROJECT_ROOT_PATH, 'resources')
