3435bd4eab2986e6d6908e8030a273eafabedead
IAM-AM <aliaksandr.manko1@gmail.com>
2023-09-02 00:11:52 +0400

    fixed resources path

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,4 +2,4 @@ import os.path
 
 FILE_PATH = os.path.abspath(__file__)
 PROJECT_ROOT_PATH = os.path.dirname(FILE_PATH)
-RESOURCE_ROOT_PATH = os.path.join(PROJECT_ROOT_PATH, 'resources')
+RESOURCE_ROOT_PATH = os.path.join(PROJECT_ROOT_PATH, '../resources')
